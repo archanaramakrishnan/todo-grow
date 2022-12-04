cca3cd724ac41ede97ec4df66492717b5309da0a
Archana Ramakrishnan <archana2359@gmail.com>
2022-12-04 15:34:16 -0500

    Filtering

--- src/App.js
+++ src/App.js
@@ -5,9 +5,26 @@ import FilterButton from "./components/FilterButton";
 import { nanoid } from "nanoid";
 import React, { useState } from "react";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   const [filter, setFilter] = useState('All');
+
+  const filterButtonList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+      />
+  ));
 
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
@@ -40,17 +57,19 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
-  const taskList = tasks.map((task) => (
-        <Todo name={task.name} 
-              completed={task.completed} 
-              id={task.id}
-              key={task.id}
-              toggleTaskCompleted={toggleTaskCompleted}
-              deleteTask={deleteTask}
-              editTask={editTask}
-            />
-          )
-        );
+  const taskList = tasks
+        .filter(FILTER_MAP[filter])
+        .map((task) => (
+            <Todo name={task.name} 
+                  completed={task.completed} 
+                  id={task.id}
+                  key={task.id}
+                  toggleTaskCompleted={toggleTaskCompleted}
+                  deleteTask={deleteTask}
+                  editTask={editTask}
+                />
+              )
+            );
 
   const tasksNoun = taskList.length === 1 ? "task" : "tasks";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
@@ -60,9 +79,7 @@ function App(props) {
       <h1>TODO: Grow</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterButtonList}
       </div>
       <h2 id="list-heading">
         {headingText}
--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -4,9 +4,14 @@ function FilterButton(props) {
     return (
         // Here, aria-pressed tells assistive technology (like screen readers) that the button can be in one of two states: pressed or unpressed. 
         // Think of these as analogs for on and off. Setting a value of true means that the button is pressed by default.
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
+        <button 
+            type="button" 
+            className="btn toggle-btn" 
+            aria-pressed={props.isPressed}
+            onClick={() => props.setFilter(props.name)}    
+        >
             <span className="visually-hidden">Show </span>
-            <span>all</span>
+            <span>{props.name}</span>
             <span className="visually-hidden"> tasks</span>
         </button>
     );
