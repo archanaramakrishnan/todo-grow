37517bd05605a41236d363b3ed5908f96eef8ec1
Archana Ramakrishnan <archana2359@gmail.com>
2022-12-04 22:25:39 -0500

    Centered tree branches!

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,3 +1,4 @@
+import { random } from "nanoid";
 import React from "react";
 import { ReactP5Wrapper } from "react-p5-wrapper";
 
@@ -12,35 +13,41 @@ export default function Tree(props) {
   
     p5.setup = () => { 
       p5.createCanvas(width, height, p5.WEBGL);
-      console.log(p5.angleMode());
+      p5.angleMode(p5.DEGREES)
+
+      p5.noLoop()
+      // console.log(p5.angleMode());
     }
   
     p5.draw = () => {
       p5.background(100)
       // translate to the center of the screen
-      p5.translate(width/2, height/2)
+      p5.translate(0, height/2)
       p5.branch(100)
-      // p5.background(250);
-      // p5.normalMaterial();
-      // p5.push();
-      // p5.rotateZ(p5.frameCount * 0.01);
-      // p5.rotateX(p5.frameCount * 0.01);
-      // p5.rotateY(p5.frameCount * 0.01);
-      // p5.plane(100);
-      // p5.pop();
     }
   
     p5.branch = (len) => {
       // console.log(props.tasks) //yay it is accessible here!
+      p5.push();
+      if(len > 10) {
 
-      //draw a line in 2D with a default width of 1 pixel
-      //the y-coordinate of the second point should be negative of the length inorder for the tree to turn in the right direction
-      p5.line(0, 0, 0, -len) 
-      //displace objects within the display window
-      //The x parameter specifies left/right translation, the y parameter specifies up/down translation.
-      p5.translate(0, -len) //translate to the end of the branch
-      p5.rotate(30)
-      p5.branch(len*0.7)
+        p5.strokeWeight(p5.map(len, 10, 100, 1, 15))
+        p5.stroke(70, 40, 20)
+        // stroke
+        //draw a line in 2D with a default width of 1 pixel
+        //the y-coordinate of the second point should be negative of the length inorder for the tree to turn in the right direction
+        p5.line(0, 0, 0, -len) 
+        //displace objects within the display window
+        //The x parameter specifies left/right translation, the y parameter specifies up/down translation.
+        p5.translate(0, -len) //translate to the end of the branch
+        p5.rotate(30)
+        p5.branch(len * 0.7)
+        
+        p5.rotate(-60)
+        p5.branch(len * 0.75)
+      }
+      p5.pop();
+      
     };
   }
 
